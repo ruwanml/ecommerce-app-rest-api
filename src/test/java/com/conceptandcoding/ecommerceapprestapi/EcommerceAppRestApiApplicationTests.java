3c2a324e7b68cca9ff5cbc156d72d8a71d0c8d82
ruwanm <ruwan.madanayake@vitalhub.com>
2024-07-31 23:50:03 +0530

    Initialize the Spring Boot Project

---
+++ src/test/java/com/conceptandcoding/ecommerceapprestapi/EcommerceAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conceptandcoding.ecommerceapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
